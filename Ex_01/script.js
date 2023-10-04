ef87853bf78df73e2ba86e148a7b2c59c6f00b84
MateoDedi <mateod@live.com>
2023-10-04 09:52:08 +0200

    first

---
+++ Ex_01/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".hello");
+const bye = document.querySelector(".bye");
+
+btn.addEventListener("click", () => {
+    alert("Oh my God! You again!!")
+});
+
+bye.addEventListener("click", () => {
+    alert("frfr!")
+});
